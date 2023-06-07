8888754809df0086dfcc0a9afe65d5421ee95cf7
miry.02 <mmumbua02@gmail.com>
2023-06-07 13:48:38 +0300

    added the responsiveness to the onCick eventlistener and made the game more interesting

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const Grid = (props) => {
-  return <div className="grid-cells">{props.value}</div>;
+  return (
+    <div
+      className={props.isHeld ? "held-grid-cells" : "grid-cells"}
+      onClick={props.holdDice}
+    >
+      {props.value}
+    </div>
+  );
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,77 @@
-import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import React, { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 import { Grid } from "./Grid";
 
 export const Main = () => {
   const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((item) => item.isHeld === true);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((item) => item.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
+      newDice.push(generateNewDice());
     }
     return newDice;
   }
-  const mapped = dice.map((item) => <Grid value={item.value} />);
-  function rollDice() {
-    setDice(allNewDice());
+
+  function holdDice(id) {
+    setDice((prevDice) =>
+      prevDice.map((item) => {
+        return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
+      })
+    );
   }
+
+  const mapped = dice.map((item) => (
+    <Grid
+      value={item.value}
+      isHeld={item.isHeld}
+      holdDice={() => holdDice(item.id)}
+      key={item.id}
+    />
+  ));
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice((prevDice) =>
+        prevDice.map((item) => {
+          return item.isHeld === true ? item : generateNewDice();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
+  }
+
   return (
     <div className="main">
+      {tenzies && <Confetti />}
       <h1>Tenzies</h1>
       <p>
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
       <div className="grid">{mapped}</div>
-      <button onClick={rollDice}>Roll</button>
+      <button onClick={rollDice}>{tenzies ? "Start Game" : "Roll"}</button>
     </div>
   );
 };
